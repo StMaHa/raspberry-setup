587d5ae728b175783b16fc37faf682f8850ea8e0
Stefan Mahnke-Hartmann <12825445+StMaHa@users.noreply.github.com>
2021-01-04 21:52:20 +0100

    Create add-linux-tools.sh

---
+++ add-linux-tools.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt -y full-upgrade
+sudo apt -y install \
+    mypaint \
+    thunderbird \
+    firefox-esr \
+    remmina \
+    rpi-imager \
+    libreoffice \
+    libreoffice-l10n-de \
+    libreoffice-help-de
