fc5e129c0228c0681c37725a44fbcc8b45085ad0
Stefan Mahnke-Hartmann <12825445+StMaHa@users.noreply.github.com>
2021-01-04 22:23:53 +0100

    Create raspberry-config.sh

---
+++ raspberry-config.sh
@@ -0,0 +1,4 @@
+sudo raspi-config nonint do_ssh 0
+sudo raspi-config nonint do_configure_keyboard de
+sudo raspi-config nonint do_wifi_country de
+sudo raspi-config nonint do_change_timezone /etc/timezone
