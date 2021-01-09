47b069f0cf73fa03887fe9e70d2561c1ce1e9cd3
Stefan Mahnke-Hartmann <12825445+StMaHa@users.noreply.github.com>
2021-01-09 21:06:33 +0100

    Update raspberry-config.sh
    Enhance boot config

--- raspberry-config.sh
+++ raspberry-config.sh
@@ -5,3 +5,6 @@ sudo raspi-config nonint do_ssh 0
 sudo raspi-config nonint do_configure_keyboard de
 sudo raspi-config nonint do_wifi_country de
 sudo raspi-config nonint do_change_timezone Europe/Berlin
+
+sudo cp rpi4-extra-config.txt /boot/extra-config.txt
+echo 'include extra-config.txt' | sudo tee -a /boot/config.txt > /dev/null
