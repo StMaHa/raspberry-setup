72905458898800fd9bbc7db73af2f9fd5761f6ca
Stefan Mahnke-Hartmann <12825445+StMaHa@users.noreply.github.com>
2021-01-09 20:58:23 +0100

    Update raspberry-config.sh
    Fix timezone and overscan

--- raspberry-config.sh
+++ raspberry-config.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
+sudo raspi-config nonint do_overscan 1
 sudo raspi-config nonint do_ssh 0
 sudo raspi-config nonint do_configure_keyboard de
 sudo raspi-config nonint do_wifi_country de
-sudo raspi-config nonint do_change_timezone /etc/timezone
+sudo raspi-config nonint do_change_timezone Europe/Berlin
