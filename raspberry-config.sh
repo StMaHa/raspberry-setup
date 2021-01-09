d3ea6196d919bedcf5dd3db38b544175ea72ba7b
Stefan Mahnke-Hartmann <12825445+StMaHa@users.noreply.github.com>
2021-01-09 21:12:30 +0100

    Update raspberry-config.sh
    Enable spi, i2c, and serial

--- raspberry-config.sh
+++ raspberry-config.sh
@@ -5,6 +5,10 @@ sudo raspi-config nonint do_ssh 0
 sudo raspi-config nonint do_configure_keyboard de
 sudo raspi-config nonint do_wifi_country de
 sudo raspi-config nonint do_change_timezone Europe/Berlin
+sudo raspi-config nonint do_memory_split 128
+sudo raspi-config nonint do_spi 0
+sudo raspi-config nonint do_i2c 0
+sudo raspi-config nonint do_serial 0
 
 sudo cp rpi4-extra-config.txt /boot/extra-config.txt
 echo 'include extra-config.txt' | sudo tee -a /boot/config.txt > /dev/null
