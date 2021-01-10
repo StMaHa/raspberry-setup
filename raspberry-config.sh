a5db905e24f656c207c8e99a58fa4a5faf3ace7f
Stefan Mahnke-Hartmann <12825445+StMaHa@users.noreply.github.com>
2021-01-10 12:16:46 +0100

    Update raspberry-config.sh
    increase swap space

--- raspberry-config.sh
+++ raspberry-config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# run raspi-config automatically
 sudo raspi-config nonint do_overscan 1
 sudo raspi-config nonint do_ssh 0
 sudo raspi-config nonint do_configure_keyboard de
@@ -10,5 +11,12 @@ sudo raspi-config nonint do_spi 0
 sudo raspi-config nonint do_i2c 0
 sudo raspi-config nonint do_serial 0
 
+# include extra boot configuration into boot config file
 sudo cp rpi4-extra-config.txt /boot/extra-config.txt
 echo 'include extra-config.txt' | sudo tee -a /boot/config.txt > /dev/null
+
+# increase size of swap file to 1024MB
+sudo dphys-swapfile swapoff
+sudo sed -i "s/\(CONF_SWAPSIZE *= *\).*/\11024/" /etc/dphys-swapfile
+sudo dphys-swapfile setup
+sudo dphys-swapfile swapon
